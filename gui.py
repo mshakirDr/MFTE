7de932223d7fd0efb8de3727ba944e3c3d487275
Muhammad Shakir <46898829+mshakirDr@users.noreply.github.com>
2022-12-17 14:27:11 +0500

    removed customtkinter for better portability

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 import tkinter
-import customtkinter
 import sys
 #https://www.reddit.com/r/Tkinter/comments/nmx0ir/how_to_show_terminal_output_in_gui/
 class Console(tkinter.Text):
@@ -19,6 +18,9 @@ class Console(tkinter.Text):
         self.config(state="normal")
         self.insert("end", content)
         self.config(state="disabled")
+        self.see(tkinter.END)
+        self.yview(tkinter.END)
+
     
     def reset(self, event):
         sys.stdout = self.old_stdout
@@ -34,27 +36,27 @@ def checkbox_event():
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app = tkinter.Tk()  # create CTk window like you do with the Tk window
 app.geometry("600x250")
 app.title("MFTE")
 # this removes the maximize button
 app.resizable(0,0)
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="Select corpus directory", command=button_function)
+button = tkinter.Button(master=app, text="Select corpus directory", command=button_function)
 button.place(x=10, y=20)
-ttr_lable = customtkinter.CTkLabel(app, text = "TTR").place(x = 400, y = 20)
+ttr_lable = tkinter.Label(app, text = "TTR").place(x = 400, y = 20)
 v = tkinter.StringVar(app, value='400')
-ttr_entry = customtkinter.CTkEntry(master=app, textvariable=v).place(x=450, y=20)
+ttr_entry = tkinter.Entry(master=app, textvariable=v).place(x=450, y=20)
 check_var = tkinter.StringVar(app, "on")
-checkbox = customtkinter.CTkCheckBox(master=app, text="Extended tags", command=checkbox_event,
+checkbox = tkinter.Checkbutton(master=app, text="Extended tags", command=checkbox_event,
                                      variable=check_var, onvalue="on", offvalue="off").place(x=250, y=20)
 
 
 frame = tkinter.Frame(app, width=580, height=170)
 frame.pack()
 frame.place(x=10, y=70)
-console = Console(master=frame).place(x=0, y=0)
+console = Console(master=frame, height=10).place(x=0, y=0)
 
 
 app.mainloop()
